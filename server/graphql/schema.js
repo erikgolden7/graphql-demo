14be85b20e99e92240093a62f921c2c41ad754fd
Erik Golden <erikgolden7@gmail.com>
2018-04-19 11:04:15 -0500

    Added schema and data model

---
+++ server/graphql/schema.js
@@ -0,0 +1,49 @@
+const {GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList} = require('graphql')
+const axios = require('axios')
+
+let users = require(`${__dirname}/model`)
+
+const PersonType = new GraphQLObjectType({
+  name: 'Person',
+  fields() {
+    return {
+      id: {
+        type: GrapQLInt,
+        resolve(person) {
+          return person.id
+        }
+      },
+      name: {
+        type: GraphQLString,
+        resolve(person) {
+          return person.name
+        }
+      },
+      height: {
+        type: GraphQLInt,
+        resolve(person) {
+          return person.height
+        }
+      }
+    }
+  }
+})
+
+const Query = new GraphQLObjectType({
+  name: 'Query',
+  fields() {
+    return {
+      people: {
+        type: new GraphQLList(PersonType),
+        // you might see root as "_", ex: resolve(_, args)
+        resolve(root, args) {
+          return users
+        }
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: Query
+})
