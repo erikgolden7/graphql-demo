113859d4a54e9fae207dd771441ed82e52e0d865
Erik Golden <erikgolden7@gmail.com>
2018-04-19 10:42:08 -0500

    boiler plate

---
+++ instructor-code/server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const graphqlHTTP = require("express-graphql");
+
+const port = 3001;
+
+const app = express();
+
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
